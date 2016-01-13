1a3b81c124c37a10343cb832067e9b5508d84b08
Ludger Heide <ludger.heide@lhtechnologies.de>
2016-01-13 18:40:21 +0100

    Added iOS Base code

--- buildProtobufs.sh
+++ buildProtobufs.sh
@@ -2,4 +2,5 @@
 /Users/ludger/Documents/Computerkram/Projekte/z_utils/generator-bin/protoc  --nanopb_out=autopilot\ \(AVR\ code\)/autopilot/ communicationProtocol.proto
 protoc --objc_out=Drone\ Control\ \(OSX\)/Drone\ Control/ communicationProtocol.proto
 protoc --objc_out=ProtoBufToCSV\ \(ObjC\)/ProtoBufToCSV/ communicationProtocol.proto
+protoc --objc_out=Drone\ Viewer\ \(iOS\)/Drone\ Viewer/ communicationProtocol.proto
 protoc --java_out=TelemetryViewer/src/ communicationProtocol.proto
