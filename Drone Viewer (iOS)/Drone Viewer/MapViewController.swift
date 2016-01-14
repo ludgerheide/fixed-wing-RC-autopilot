0bb2a95dc6308a8cfc8e890822f2c8af260f81d7
Ludger Heide <ludger.heide@lhtechnologies.de>
2016-01-14 18:23:28 +0100

    Added basic components

---
+++ Drone Viewer (iOS)/Drone Viewer/MapViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MapViewController.swift
+//  Drone Viewer
+//
+//  Created by Ludger Heide on 13.01.16.
+//  Copyright © 2016 Ludger Heide. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    @IBAction func showMapSettingsPopover(sender: AnyObject) {
+//        MyViewController* content = [[MyViewController alloc] init];
+//        UIPopoverController* aPopover = [[UIPopoverController alloc]
+//        initWithContentViewController:content];
+//        aPopover.delegate = self;
+//        
+//        // Store the popover in a custom property for later use.
+//        self.popoverController = aPopover;
+//        
+//        [self.popoverController presentPopoverFromBarButtonItem:sender
+//        permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
