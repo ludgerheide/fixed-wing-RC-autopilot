d38409ac30230dd99d142c6ccb6061f2b9035c11
Ludger Heide <ludger.heide@lhtechnologies.de>
2016-01-18 22:58:18 +0100

    Small changes to buiuld script and gitignore

--- buildProtobufs.sh
+++ buildProtobufs.sh
@@ -4,3 +4,4 @@ protoc --objc_out=Drone\ Control\ \(OSX\)/Drone\ Control/ communicationProtocol.
 protoc --objc_out=ProtoBufToCSV\ \(ObjC\)/ProtoBufToCSV/ communicationProtocol.proto
 protoc --objc_out=Drone\ Viewer\ \(iOS\)/Drone\ Viewer/ communicationProtocol.proto
 protoc --java_out=TelemetryViewer/src/ communicationProtocol.proto
+protoc --java_out=DroneComms/src/ communicationProtocol.proto
