914d17bfcf98472039fc40ee721513808c47d7c5
Ludger Heide <ludger.heide@lhtechnologies.de>
2017-08-15 14:37:24 +0800

    Updated paths in script

--- buildProtobufs.sh
+++ buildProtobufs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/Users/ludger/Documents/Computerkram/Projekte/z_utils/generator-bin/protoc  --nanopb_out=autopilot\ \(AVR\ code\)/autopilot/ communicationProtocol.proto
+protoc-nanopb  --nanopb_out=autopilot\ \(AVR\ code\)/autopilot/ communicationProtocol.proto
 protoc --objc_out=Drone\ Control\ \(OSX\)/Drone\ Control/ communicationProtocol.proto
 protoc --objc_out=ProtoBufToCSV\ \(ObjC\)/ProtoBufToCSV/ communicationProtocol.proto
 protoc --objc_out=Drone\ Viewer\ \(iOS\)/Drone\ Viewer/ communicationProtocol.proto
